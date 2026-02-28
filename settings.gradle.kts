8a6e4dc8a49eb47360caa3d1c67fa193bb2aaf9e
jose-almir <jr.cod.dev@gmail.com>
2026-02-27 21:40:26 -0300

    chore: update project metadata and base package

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "dev.hytalemodding"
+rootProject.name = "com.almirdev.simplerelics"
 
 plugins {
     // See documentation on https://scaffoldit.dev
@@ -21,8 +21,8 @@ hytale {
     }
 
     manifest {
-        Group = "HytaleModding"
-        Name = "ExamplePlugin"
-        Main = "dev.hytalemodding.ExamplePlugin"
+        Group = "com.almirdev.simplerelics"
+        Name = "SimpleRelics"
+        Main = "com.almirdev.simplerelics.SimpleRelicsPlugin"
     }
 }
---
+++ src/main/java/com/almirdev/simplerelics/SimpleRelicsPlugin.java
@@ -0,0 +1,19 @@
+package com.almirdev.simplerelics;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+
+import javax.annotation.Nonnull;
+
+public class SimpleRelicsPlugin extends JavaPlugin {
+
+    public SimpleRelicsPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+}
