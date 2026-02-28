e219618f8de7bf5827b245064fd53c0f73ef3f93
jose-almir <jr.cod.dev@gmail.com>
2026-02-27 22:16:16 -0300

    feat: implement base damage and interceptor mechanics

---
+++ src/main/java/com/almirdev/simplerelics/common/EmeraldCrossRelicInterceptor.java
@@ -0,0 +1,28 @@
+package com.almirdev.simplerelics.common;
+
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.modules.entity.damage.Damage;
+import com.hypixel.hytale.server.core.modules.entitystats.EntityStatValue;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+public class EmeraldCrossRelicInterceptor extends FatalDamageInterceptor {
+    public static final String RELIC_ID = "SimpleRelics_EmeraldCross";
+
+    public EmeraldCrossRelicInterceptor() {
+        super(RELIC_ID);
+    }
+
+    @Override
+    public FatalDamageResult apply(Player player, Damage damage, EntityStatValue health, Store<EntityStore> store, Ref<EntityStore> ref) {
+        damage.setCancelled(true);
+
+        return new FatalDamageResult(
+                health.getMax() * 0.5f,
+                "SFX_Divine_Respawn",
+                "I SAVED YOU",
+                "You are so lucky..."
+        );
+    }
+}
---
+++ src/main/java/com/almirdev/simplerelics/common/FatalDamageResult.java
@@ -0,0 +1,4 @@
+package com.almirdev.simplerelics.common;
+
+public record FatalDamageResult(float restoreAmount, String soundId, String notificationTitle, String notificationSubtitle) {
+}
