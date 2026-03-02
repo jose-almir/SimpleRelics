bcfb944cadeccc234df1c85425814b56ee9c5cd3
jose-almir <jr.cod.dev@gmail.com>
2026-03-02 20:14:16 -0300

    feat: configure relic damage system with proper dependencies and damage group

--- src/main/java/com/almirdev/simplerelics/system/SimpleRelicsDamageSystem.java
+++ src/main/java/com/almirdev/simplerelics/system/SimpleRelicsDamageSystem.java
@@ -3,23 +3,29 @@ package com.almirdev.simplerelics.system;
 import com.almirdev.simplerelics.common.Relic;
 import com.almirdev.simplerelics.common.RelicContext;
 import com.almirdev.simplerelics.common.RelicRegistry;
-import com.hypixel.hytale.component.ArchetypeChunk;
-import com.hypixel.hytale.component.CommandBuffer;
-import com.hypixel.hytale.component.Ref;
-import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.*;
+import com.hypixel.hytale.component.dependency.Dependency;
+import com.hypixel.hytale.component.dependency.Order;
+import com.hypixel.hytale.component.dependency.SystemDependency;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.EntityEventSystem;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
 import com.hypixel.hytale.server.core.modules.entity.damage.Damage;
+import com.hypixel.hytale.server.core.modules.entity.damage.DamageModule;
+import com.hypixel.hytale.server.core.modules.entity.damage.DamageSystems;
 import com.hypixel.hytale.server.core.modules.entitystats.EntityStatMap;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
+import java.util.Set;
+
 public class SimpleRelicsDamageSystem extends EntityEventSystem<EntityStore, Damage> {
     public static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+    private static final Set<Dependency<EntityStore>> DEPENDENCIES = Set.of(new SystemDependency<>(Order.BEFORE, DamageSystems.FallDamagePlayers.class));
+
 
     public SimpleRelicsDamageSystem() {
         super(Damage.class);
@@ -29,6 +35,18 @@ public class SimpleRelicsDamageSystem extends EntityEventSystem<EntityStore, Dam
     @Override
     public Query<EntityStore> getQuery() {
         return Query.and(Player.getComponentType());
+    }
+
+    @NullableDecl
+    @Override
+    public SystemGroup<EntityStore> getGroup() {
+        return DamageModule.get().getGatherDamageGroup();
+    }
+
+    @NonNullDecl
+    @Override
+    public Set<Dependency<EntityStore>> getDependencies() {
+        return DEPENDENCIES;
     }
 
     @Override
@@ -48,9 +66,16 @@ public class SimpleRelicsDamageSystem extends EntityEventSystem<EntityStore, Dam
         }
 
         ItemStack utilityItem = player.getInventory().getUtilityItem();
-        if (utilityItem == null) return;
+        if (utilityItem == null) {
+            LOGGER.atInfo().log("Player is not equipping utility item.");
+            return;
+        }
 
+        LOGGER.atInfo().log("Player is equipping utility item. Trying to activate effects.");
         Relic relic = RelicRegistry.get(utilityItem.getItemId());
+
+        if(relic == null) return;
+
         RelicContext context = new RelicContext(player, damage, stats, store, ref, commandBuffer);
 
         relic.tryActivate(context, utilityItem);
