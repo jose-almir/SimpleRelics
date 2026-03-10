8ad8921973c398a2b68d0fda2dced0f23992abbd
jose-almir <jr.cod.dev@gmail.com>
2026-03-10 14:21:26 -0300

    feat: increase immune effect time

--- src/main/java/com/almirdev/simplerelics/common/Relics.java
+++ src/main/java/com/almirdev/simplerelics/common/Relics.java
@@ -24,7 +24,7 @@ public class Relics {
                             ),
                             ApplyEntityEffect.toSelf(
                                     "Immune",
-                                    3,
+                                    4,
                                     OverlapBehavior.OVERWRITE
                             ),
                             new NotificationEffect("items.SimpleRelics_EmeraldCross.notification_title", "items.SimpleRelics_EmeraldCross.notification_subtitle", Color.GREEN),
